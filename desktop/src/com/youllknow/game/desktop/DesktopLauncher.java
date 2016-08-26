0b51203fbe39c3390a1cc4381f905321f2d71ec3
Seurimas <nicholasanderson5@gmail.com>
2016-08-26 18:11:22 -0500

    Basic LibGDX base project.

---
+++ desktop/src/com/youllknow/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.youllknow.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.youllknow.game.LudumDare36Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumDare36Game(), config);
+	}
+}
---
+++ html/src/com/youllknow/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.youllknow.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.youllknow.game.LudumDare36Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LudumDare36Game();
+        }
+}
